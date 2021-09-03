d3a414686f312deae0606008889ddae78a265ea9
Eduardo Pinho <86359636+ShyrTheFirst@users.noreply.github.com>
2021-09-03 09:03:25 -0300

    atualização 0.0.1.0
    novas escolhas do teste de objetivo corrigido

--- niveis_objetivo.py
+++ niveis_objetivo.py
@@ -1,13 +1,14 @@
 import random
-nivel_objetivo_quartel = random.randint(2,6)
+nivel_objetivo_quartel = 0
 evoluirquartel = "evoluir quarteis em %s" %nivel_objetivo_quartel
-nivel_objetivo_estabulo = random.randint(2,6)
+nivel_objetivo_estabulo = 0
 evoluirestabulo = "evoluir estabulo em %s" %nivel_objetivo_estabulo
-nivel_objetivo_expandir = random.randint(3,7)
+nivel_objetivo_expandir = 0
 expandirx = "expandir as suas ligações em %s" %nivel_objetivo_expandir
-nivel_objetivo_treinar = random.randint(10,50)
+nivel_objetivo_treinar = 0
 treinarx = "Treinar %s soldados" %nivel_objetivo_treinar
 termos_de_guerra = (evoluirquartel, evoluirestabulo, expandirx, treinarx)
 objetivo = random.choice(termos_de_guerra)
+
 
 
--- teste_objetivo.py
+++ teste_objetivo.py
@@ -8,6 +8,18 @@ import easygui as box
 # Iniciar o Objetivo           #
 ################################
 def iniciar_objetivo():
+ obj.nivel_objetivo_quartel = random.randint(2,6)
+ obj.evoluirquartel = "evoluir quarteis em %s" %obj.nivel_objetivo_quartel
+ obj.nivel_objetivo_estabulo = random.randint(2,6)
+ obj.evoluirestabulo = "evoluir estabulo em %s" %obj.nivel_objetivo_estabulo
+ obj.nivel_objetivo_expandir = random.randint(3,7)
+ obj.expandirx = "expandir as suas ligações em %s" %obj.nivel_objetivo_expandir
+ obj.nivel_objetivo_treinar = random.randint(10,50)
+ obj.treinarx = "Treinar %s soldados" %obj.nivel_objetivo_treinar
+ obj.termos_de_guerra = (obj.evoluirquartel, obj.evoluirestabulo, obj.expandirx, obj.treinarx)
+ print(obj.termos_de_guerra)
+ obj.objetivo = random.choice(obj.termos_de_guerra)
+ print(obj.objetivo)
  aceitar_destino = box.ynbox("Seu objetivo é: %s. Você deve realizar o seu objetivo dentro de %s turnos para conquistar vantagens estratégicas. Aceita seu Destino?" %(obj.objetivo, nv.turnos_aleatorios), "Aceitar o seu Destino", ("Sim", "Não"))
  if aceitar_destino == True:
   iniciar_jogo = True
@@ -16,7 +28,7 @@ def iniciar_objetivo():
   box.msgbox("A escolha e toda sua. Até a próxima!", "Saindo")
   iniciar_jogo = False
   iniciar_objetivo = False
-  
+
 ################################
 # Teste do Objetivo            #
 ################################
@@ -27,22 +39,22 @@ def testar_objetivo():
         box.msgbox("Você alcançou o objetivo, por isso irá ganhar uma recompensa!")
         #criar recompensa
     else:
-        box.msgbox("Você não alcançou o objetivo, infelizmente não receberá uma recompensa, o seu nivel: %s , o nivel necessário %s" %(nv.nv_quartel, obj.nivel_objetivo_quartel)
+        box.msgbox("Você não alcançou o objetivo, infelizmente não receberá uma recompensa, o seu nivel: %s , o nivel necessário %s" %(nv.nv_quartel, obj.nivel_objetivo_quartel))
  if obj.objetivo == obj.evoluirestabulo:
     if nv.nv_estabulo == obj.nivel_objetivo_estabulo:
         box.msgbox("Você alcançou o objetivo, por isso irá ganhar uma recompensa!")
         #criar recompensa
     else:
-        box.msgbox("Você não alcançou o objetivo, infelizmente não receberá uma recompensa")
+        box.msgbox("Você não alcançou o objetivo, infelizmente não receberá uma recompensa o seu nivel: %s , o nivel necessário %s" %(nv.nv_estabulo, obj.nivel_objetivo_estabulo))
  if obj.objetivo == obj.treinarx:
     if nv.num_soldados == obj.nivel_objetivo_treinar:
         box.msgbox("Você alcançou o objetivo, por isso irá ganhar uma recompensa!")
         #criar recompensa
     else:
-        box.msgbox("Você não alcançou o objetivo, infelizmente não receberá uma recompensa")
+        box.msgbox("Você não alcançou o objetivo, infelizmente não receberá uma recompensa o seu nivel: %s , o nivel necessário %s" %(nv.num_soldados, obj.nivel_objetivo_treinar))
  if obj.objetivo == obj.expandirx:
     if nv.diplomacia == obj.nivel_objetivo_expandir:
         box.msgbox("Você alcançou o objetivo, por isso irá ganhar uma recompensa!")
         #criar recompensa
     else:
-        box.msgbox("Você não alcançou o objetivo, infelizmente não receberá uma recompensa")
+        box.msgbox("Você não alcançou o objetivo, infelizmente não receberá uma recompensa o seu nivel: %s , o nivel necessário %s" %(nv.diplomacia, obj.nivel_objetivo_expandir))
